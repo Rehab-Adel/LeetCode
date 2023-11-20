23bf3b5cc940b283ed98324cee4cad2dc2dcdf5b
Rehab Adel <98564966+Rehab-Adel@users.noreply.github.com>
2023-11-20 03:30:54 +0200

    Create 67.Add-Binary.cpp

---
+++ 67.Add-Binary.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string result = "";
+        int carry = 0;
+
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int bitA = (i >= 0) ? a[i--] - '0' : 0;
+            int bitB = (j >= 0) ? b[j--] - '0' : 0;
+
+            int sum = bitA + bitB + carry;
+            carry = sum / 2;
+
+            result = to_string(sum % 2) + result;
+        }
+
+        return result;
+    }
+};
