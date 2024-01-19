0630febb1b5f615076d78f7230a0452a0c26db5c
Rehab Adel <98564966+Rehab-Adel@users.noreply.github.com>
2024-01-19 22:45:36 +0200

    Time: 101 ms (99.33%) | Memory: 81 MB (16.22%) - LeetSync

---
+++ CPP/219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> indexMap;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (indexMap.find(nums[i]) != indexMap.end() && abs(i - indexMap[nums[i]]) <= k) {
+                return true;
+            }
+            indexMap[nums[i]] = i;
+        }
+
+        return false;
+    }
+};
