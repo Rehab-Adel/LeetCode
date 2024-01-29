977ca1ffbeed8b384b31c6c4fb582c206bfcf1df
Rehab Adel <98564966+Rehab-Adel@users.noreply.github.com>
2024-01-29 18:42:30 +0200

    Time: 121 ms (90.66%) | Memory: 49.4 MB (54.52%) - LeetSync

---
+++ CPP/167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[] TwoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.Length - 1;
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return new int[] { left + 1, right + 1 };
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return new int[0];
+    }
+}
