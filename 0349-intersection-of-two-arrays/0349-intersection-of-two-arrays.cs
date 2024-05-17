2e56e976ce482e260c7f2b7a757797c041149ff4
Rehab Adel <98564966+Rehab-Adel@users.noreply.github.com>
2024-05-18 02:14:30 +0300

    Time: 110 ms (29.05%), Space: 46.3 MB (49.53%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cs
@@ -0,0 +1,8 @@
+public class Solution {
+    public int[] Intersection(int[] nums1, int[] nums2) {
+        HashSet<int> set1 = new HashSet<int>(nums1);
+        HashSet<int> set2 = new HashSet<int>(nums2);
+        set1.IntersectWith(set2);
+        return set1.ToArray();
+    }
+}
